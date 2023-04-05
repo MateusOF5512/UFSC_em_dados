61a0bbb99185471a6f43bea54e5df04a4f390b85
M-Ortiz <69127852+MateusOF5512@users.noreply.github.com>
2023-04-05 14:58:14 -0300

    Add files via upload

--- 1_main.py
+++ 1_main.py
@@ -7,41 +7,77 @@ from plots.plots import *
 from layout.layout import *
 
 im = Image.open("image/ufsc.jpg")
+im2 = Image.open("image/brasao.png")
 st.set_page_config(page_title="UFSC em Dados", page_icon=im, layout="wide")
 
 st.markdown(""" <style>
         footer {visibility: hidden;}
         </style> """, unsafe_allow_html=True)
 
-df = load_google_sheet()
 
-
-col1, col2, col3 = st.columns([300, 800, 300])
-with col1:
-    st.text('')
-with col2:
-    st.markdown("<h1 style='font-size:250%; text-align: center; color: #05A854; padding: 0px 0px'" +
+st.markdown("<h1 style='font-size:250%; text-align: center; color: #05A854; padding: 0px 0px;'" +
                 ">UFSC em Dados</h1>", unsafe_allow_html=True)
-    st.markdown("<h3 style='font-size:100%; text-align: center; color: #05A854; padding: 0px 0px 10px 0px;;'" +
-                ">Última atualização: 01/04/2023</h3>", unsafe_allow_html=True)
-with col3:
-    basedados = st.selectbox("Base de dados da UFSC:",
-                         options=["População da UFSC", "Vagas por Curso", 'Inscritos por Curso'], index=0)
+st.markdown("<h3 style='font-size:100%; text-align: center; color: #05A854; padding: 0px 0px 10px 0px;'" +
+            ">Última atualização: 01/04/2023</h3>", unsafe_allow_html=True)
 
+
+with st.sidebar:
+    col1, col2, col3 = st.columns([2, 3, 2])
+    with col1:
+        st.write('')
+    with col2:
+        st.image(im2, caption='Universidade Federal de Santa Catarina')
+    with col3:
+        st.write('')
+
+    st.markdown('---')
+    basedados = st.selectbox("Selecione a base de dados para sua análise:",
+                             options=["População de Estudantes",
+                                      "População de Funcionários",
+                                      "Vagas no Vestibular por Curso",
+                                      'Inscritos no Vestibular por Curso'], index=0)
+
+    if basedados == "População de Estudantes":
+        df1 = load_google_sheet(tabela="1")
+    elif basedados == "População de Funcionários":
+        df1 = load_google_sheet(tabela="2")
+
+    st.markdown('---')
+
+    with st.expander("🎲️ Filtrar os dados"):
+        ano_max = int(df1['ANO'].max())
+        ano_min = int(df1['ANO'].min())
+        ano_range_min, ano_range_max, = st.slider('Selecione os ANOS da análise temporal:',
+                                                  min_value=ano_min, max_value=ano_max, value=(ano_min, ano_max))
+
+        mask_valor = (df1['ANO'] >= ano_range_min) & (df1['ANO'] <= ano_range_max)
+
+        colunas = df1.columns.unique().tolist()
+        selected_bairro = st.multiselect("Selecione as coluna da base de dados:",
+                                         options=colunas, default=colunas)
+        st.markdown('---')
+
+
+df = df1.loc[mask_valor]
+df =df1
 
 st.markdown('---')
-st.markdown("<h2 style='font-size:150%; text-align: center; color: #05A854; padding: 0px 0px 20px 0px;'" +
-                ">Analise Temporal 1980 a 2021: "+basedados+" - Tabela Dinâmica</h2>", unsafe_allow_html=True)
+
+st.markdown("<h2 style='font-size:150%; text-align: center; color: #05A854; padding: 0px 0px 15px 0px;'" +
+            ">"+basedados+" entre "+str(ano_range_min)+" a "+str(ano_range_max)+" - Tabela Dinâmica</h2>", unsafe_allow_html=True)
 
 selected_rows = agg_tabela(df, True)
 
+st.text('')
 tab1, tab2, tab3, tab4 = st.tabs(["‍🔬 LABORATÓRIO", "🔎 ANALISE EXPLORATÓRIA", "📊 DASHBOARD" , '👶 PRIMEIRA VEZ AQUI?'])
 
 with tab1:
     if len(selected_rows) == 0:
-        parte1(df, selected_rows)
+        estudantes(df, selected_rows)
+        st.text('')
     elif len(selected_rows) != 0:
-        parte1(selected_rows, selected_rows)
+        estudantes(selected_rows, selected_rows)
+        st.text('')
 with tab2:
     if len(selected_rows) == 0:
         relatorio(df)
